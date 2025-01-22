d16edd33aa628430306b7cd714fd1b4b5ef26284
John Alexander Diaz Betancur <diazbetancur@gmail.com>
2025-01-22 18:09:38 -0500

    add logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,7 @@ import { jwtDecode } from 'jwt-decode';
 })
 export class AuthService {
   private readonly TOKEN_KEY = 'Token';
+  private userData: any = null;
 
   constructor(private readonly router: Router) {}
 
@@ -21,10 +22,12 @@ export class AuthService {
 
   saveToken(token: string): void {
     localStorage.setItem(this.TOKEN_KEY, token);
+    this.setUserData(token);
   }
 
   logout(): void {
     localStorage.removeItem(this.TOKEN_KEY);
+    this.clearUserData();
     this.router.navigate(['/login']);
   }
 
@@ -37,5 +40,30 @@ export class AuthService {
       console.error('Error decoding token:', error);
       return true;
     }
+  }
+  private setUserData(token: string): void {
+    try {
+      const decoded: any = jwtDecode(token);
+      console.log(decoded);
+      sessionStorage.setItem('userData', JSON.stringify(decoded));
+    } catch (error) {}
+  }
+
+  public getUserData(): any {
+    if (!this.userData) {
+      const data = sessionStorage.getItem('userData');
+      this.userData = data ? JSON.parse(data) : null;
+    }
+    return this.userData;
+  }
+
+  public getUserField(field: string): any {
+    const user = this.getUserData();
+    return user ? user[field] : null;
+  }
+
+  private clearUserData(): void {
+    this.userData = null;
+    sessionStorage.removeItem('userData');
   }
 }
